c21cd3330822a877f0daba500b2c7f683bb671fe
Mathias Forsman <gusmathfo@student.gu.se>
2021-02-03 19:31:34 +0100

    Pushed and slightly modified Z's commit

---
+++ Lab1/Views.sql
@@ -0,0 +1,17 @@
+CREATE VIEW BasicInformation AS
+SELECT  idnr, Students.name, login, Students.program, StudentBranches.branch 
+FROM Students
+LEFT JOIN StudentBranches
+ON StudentBranches.program = Students.program AND
+    StudentBranches.student = Students.idnr;
+
+
+CREATE VIEW FinishedCourses AS
+SELECT student, course, grade, credits
+FROM Courses, Taken
+WHERE course = code;
+
+CREATE VIEW PassedCourses AS 
+SELECT student, course, credits 
+FROM Taken, Courses 
+WHERE grade <> 'U' AND course = code;
