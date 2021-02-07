2dc0e3cff74329fbd41e0e1eee79e6bbd4e53e93
Mathias Forsman <gusmathfo@student.gu.se>
2021-02-07 19:52:15 +0100

    added test and ran it, assignment done

--- Lab1/Drop_tables.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS Students, Branches, Courses,
-    LimitedCourses, StudentBranches, Classifications, 
-    Classified, MandatoryProgram, MandatoryBranch, 
-    RecommendedBranch, Registered, Taken, WaitingList
-    CASCADE;
