4c34babd6961f0f6eb41415887cddf7675b9b724
Zakaria <44900079+HersiZa@users.noreply.github.com>
2021-03-05 16:06:03 +0100

    Update tests.sql

--- Lab3/tests.sql
+++ Lab3/tests.sql
@@ -1,68 +1,81 @@
-\i startup.sql
+
 -- Registered to an unlim Course
-INSERT INTO Registrations VALUES ('1111111111', 'CCC111'); --No problem, "Insert 0 1"
--- Registered succesfully
+INSERT INTO Registrations VALUES ('1111111111', 'CCC111');
 SELECT * FROM Registrations;
 
 -- Register to limited course 
-INSERT INTO Registrations VALUES ('2222222222','CCC222'); --2, regged succesfully
-
+INSERT INTO Registrations VALUES ('2222222222','CCC222'); 
 SELECT * FROM Registrations;
 
--- Register and going into wait list
-INSERT INTO Registrations VALUES ('1111111111','CCC222'); --1  waiting succesfully
 
-INSERT INTO Registrations VALUES ('3333333333','CCC222'); --1 waiting succesfully
+INSERT INTO Registrations VALUES ('1111111111','CCC222'); 
 
+INSERT INTO Registrations VALUES ('3333333333','CCC222'); 
 SELECT * FROM Registrations;
 
 --Register several times 
-INSERT INTO Registrations VALUES ('1111111111','CCC111'); --ERROR: the student is already registered in this course, welps
+INSERT INTO Registrations VALUES ('1111111111','CCC111'); 
 
-INSERT INTO Registrations VALUES ('2222222222','CCC222'); -- ERROR: the student is already registered in this course, welps
+INSERT INTO Registrations VALUES ('2222222222','CCC222'); 
 
+--Try register if in WL
+
+INSERT INTO Registrations VALUES ('1111111111','CCC222'); 
+SELECT * FROM Registrations;
+
+--Try register for a taken course (Passed) FIXA!!!
+INSERT INTO Taken VALUES('4444444444','CCC222','5');
+INSERT INTO Registrations VALUES ('4444444444','CCC222');
+SELECT * FROM Registrations;
+
+--Try register for a taken course (Failed) SAKNAR TRIGGERN
+INSERT INTO Taken VALUES('333333333','CCC222','U');
+INSERT INTO Registrations VALUES ('3333333333','CCC222');
 SELECT * FROM Registrations;
 
 
 -- prerequisite not met
-INSERT INTO Registrations VALUES ('1111111111','CCC333'); -- ERROR: Prerequisites has not been fulfilled
-
+INSERT INTO Registrations VALUES ('1111111111','CCC333');  -- KLaga
+SELECT * FROM Registrations;
 
 -- Prerequisite met
 INSERT INTO Taken VALUES('1111111111','CCC222','5');
 INSERT INTO Registrations VALUES ('1111111111','CCC333');
-SELECT * FROM Registrations;  -- Registered succesfully
+SELECT * FROM Registrations;
+
+DELETE FROM WaitingList WHERE student = '3333333333' AND Course = 'CCC222'; --Positionen förändras ej (no update)
 
 
---------------------------------------------------------------^^ FOR first function and trigger (above)
 -- Unregistered from unlim course
-DELETE FROM Registrations WHERE student = '1111111111' AND course = 'CCC111';  --Deleted succesfully
-
+DELETE FROM Registrations WHERE student = '1111111111' AND course = 'CCC111';  
 SELECT * FROM Registrations;
 
 --Unregister from limited course 
-DELETE FROM Registrations WHERE student = '1111111111' AND course = 'CCC333';  --Deleted succesfully
-
+DELETE FROM Registrations WHERE student = '1111111111' AND course = 'CCC333';  
 SELECT * FROM Registrations;
 
 --Unregister from WaitingList
-DELETE FROM Registrations WHERE student = '1111111111' AND course = 'CCC222';  --Deleted succesfully
-
+DELETE FROM Registrations WHERE student = '1111111111' AND course = 'CCC222';  
 SELECT * FROM Registrations;
 
 --Unergister from registered 
-DELETE FROM Registrations WHERE student = '2222222222' AND course = 'CCC222';  --Deleted succesfully
-
+DELETE FROM Registrations WHERE student = '2222222222' AND course = 'CCC222'; 
 SELECT * FROM Registrations;
 
 --unregister from an overfull course with the WaitingList
-INSERT INTO Registered VALUES ('2222222222','CCC222'); --Inserted succsessfully
+INSERT INTO Registered VALUES ('2222222222','CCC222'); 
+SELECT * FROM Registrations;
 
-INSERT INTO Registrations VALUES ('1111111111','CCC222');  --Inserted succsessfully
+INSERT INTO Registrations VALUES ('1111111111','CCC222'); 
+SELECT * FROM Registrations;
 
-DELETE FROM Registrations WHERE student = '2222222222' AND course = 'CCC222';  --Deleted succesfully, 
+DELETE FROM Registrations WHERE student = '2222222222' AND course = 'CCC222';   
+SELECT * FROM Registrations;
 
-SELECT * FROM Registrations; 
+
+
+
+
 
 
 
