7f8d7c74fe9b4f2260b0314241df28b9c0eaca83
hersiz <hersiz@student.chalmers.se>
2021-03-10 15:20:22 +0100

    Commit

--- Lab4/src/PortalConnection.java
+++ Lab4/src/PortalConnection.java
@@ -1,6 +1,12 @@
 
+import netscape.javascript.JSObject;
+
+import javax.swing.*;
 import java.sql.*; // JDBC stuff.
 import java.util.Properties;
+import org.json.*;
+import java.sql.Statement;
+
 
 public class PortalConnection {
 
@@ -14,9 +20,9 @@ public class PortalConnection {
     // static final String USERNAME = "tda357_nnn";
     // static final String PASSWORD = "yourPasswordGoesHere";
 
-
+    java.sql.Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/lab1tda357", "postgres","postgres");
     // This is the JDBC connection object you will be using in your methods.
-    private Connection conn;
+  //  private Connection conn;
 
     public PortalConnection() throws SQLException, ClassNotFoundException {
         this(DATABASE, USERNAME, PASSWORD);  
@@ -37,17 +43,16 @@ public class PortalConnection {
 
         int result = 0;
 
-        //Create a statement using connection object
-        PreparedStatement statement = conn.prepareStatement("INSERT INTO Registrations (Student, Course) VALUES (?, ?);");
+       String Sql_regiter = "INSERT INTO Registrations (?,?)";
+
+        PreparedStatement statement = conn.prepareStatement(Sql_regiter); //Om error sätt in i Try Claus
         try {
-            //Connection connection = DriverManager.getConnection(DATABASE, USERNAME, PASSWORD);
-            //System.out.println("Connected to PostgresSQL server"); //Kommer ej hit
             statement.setString(1, student);
             statement.setString(2, courseCode);
 
             System.out.println(statement);
 
-            result = statement.executeUpdate();
+            result =  statement.executeUpdate(); //Shows how many rows were affected
             if (result > 0){
                 System.out.println("Insertion Okay");
             }
@@ -63,20 +68,28 @@ public class PortalConnection {
     }
       
 
-
-
     // Unregister a student from a course, returns a tiny JSON document (as a String)
     public String unregister(String student, String courseCode) throws SQLException {
-        PreparedStatement statement = conn.prepareStatement("DELETE FROM Registrations WHERE = ? AND COURSE = ?");
+        int result = 0;
+
+
+
+       String Sql_unregister = "DELETE FROM Registrations WHERE student = "+ student +" AND course = " + courseCode +"";
+       // String Sql_unregister = "DELETE FROM Registrations WHERE student = ? AND course = ?";
+       PreparedStatement statement = conn.prepareStatement(Sql_unregister);
+
+       // String sql_unregister = "DELETE FROM Registrations WHERE student = ? AND course = ?";
+
+       // Statement statement = conn.createStatement(Sql_unregister);
+
 
         try{
-
             statement.setString(1, student);
             statement.setString(2,courseCode);
 
             System.out.println(statement);
 
-            statement.executeUpdate();
+            result = statement.executeUpdate();//How many rows were affected
 
             return "{\"success\":true}";
 
@@ -89,21 +102,95 @@ public class PortalConnection {
     // Return a JSON document containing lots of information about a student, it should validate against the schema found in information_schema.json
     public String getInfo(String student) throws SQLException{
         
-        try(PreparedStatement st = conn.prepareStatement(
-            // replace this with something more useful
-            "SELECT jsonb_build_object('student',idnr,'name',name) AS jsondata FROM BasicInformation WHERE idnr=?"//!!!!
-            );){
+        String Sql_BasicInformation =   "SELECT  idnr, Students.name, login, Students.program, StudentBranches.branch \n" +
+                                         "FROM Students\n" +
+                                          "LEFT JOIN StudentBranches\n" +
+                                           "ON (StudentBranches.program = Students.program AND \n" +
+                                            " StudentBranches.student = Students.idnr) WHERE Students.idnr = ?";
+
+
+        String Sql_Taken            = "SELECT student, course, grade, credits\n" +
+                                        "FROM Courses, Taken\n" +
+                                         "WHERE course = code AND student = ?";
+
+
+        String Sql_RegStatus        = "SELECT student, course, 'registered' as status\n" +
+                                        " FROM Registered \n" +
+                                          "UNION\n" +
+                                           "SELECT student, course, 'waiting' as status\n" +
+                                             "FROM WaitingList WHERE student = ?";
+
+        String Sql_PathToGrad       = "SELECT * FROM PathToGraduation WHERE student = ?";
+
+
+        JSONObject json = new JSONObject();
+
+        PreparedStatement psSQLBasicInfo = conn.prepareStatement(Sql_BasicInformation);
+
+        try(psSQLBasicInfo){
+
+            psSQLBasicInfo.setString(1, student);
             
-            st.setString(1, student);
+            ResultSet resultset = psSQLBasicInfo.executeQuery();
             
-            ResultSet rs = st.executeQuery();
-            
-            if(rs.next())
-              return rs.getString("jsondata");
-            else
-              return "{\"student\":\"does not exist :(\"}"; 
-            
-        } 
+            if(resultset.next() != false){
+                json.put("student", resultset.getString("idnr"));
+                json.put("Students name", resultset.getString("name"));
+                json.put("login", resultset.getString("login"));
+                json.put("Students program", resultset.getString("program"));
+                json.put("branch", resultset.getString("branch")); //Osäker på vad händer ifall branch är tom?
+
+            }
+            JSONObject course = new JSONObject();
+            JSONArray Taken = new JSONArray();
+
+            PreparedStatement psSQL = conn.prepareStatement(Sql_Taken);
+            psSQL.setString(1,student);
+            resultset = psSQL.executeQuery();
+
+            while (resultset.next()){ //KSK WHILE
+                course.put("student", resultset.getString("student"));
+                course.put("course",resultset.getString("course"));
+                course.put("grade",resultset.getInt("grade"));
+                course.put("credits",resultset.getString("credits"));
+
+                Taken.put(course);
+            }
+            json.put("Finished", Taken);
+
+
+            JSONArray regged = new JSONArray();
+
+            psSQL = conn.prepareStatement(Sql_RegStatus);
+            psSQL.setString(1, student);
+            resultset = psSQL.executeQuery();
+
+            if (resultset.next()){
+                course.put("course",resultset.getString("course"));
+                course.put("status", resultset.getString("status"));
+                regged.put(course);
+            }
+
+            json.put("Registered", regged);
+
+            psSQL = conn.prepareStatement(Sql_PathToGrad);
+            psSQL.setString(1, student);
+            resultset = psSQL.executeQuery();
+
+            if (resultset.next()){
+                json.put("isQualified", resultset.getBoolean("qualified"));
+                json.put("totalCredits", resultset.getFloat("totalCredits"));
+                json.put("researchCredits", resultset.getFloat("researchCredits"));
+                json.put("mathCredits", resultset.getFloat("mathCredits"));
+                json.put("mandatoryLeft", resultset.getInt("mandatoryLeft"));
+                json.put("seminarCourses", resultset.getInt("seminarCourses"));
+                //json.put("recommendedCredits", resultset.getFloat("recommendedCredits"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json.toString();
     }
 
     // This is a hack to turn an SQLException into a JSON string error message. No need to change.
